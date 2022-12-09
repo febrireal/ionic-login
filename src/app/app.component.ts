374669fd2e309612b8be9ebe310a605439c49799
febri reinaldi setijawan <febrisetijawan26@gmail.com>
2022-12-09 17:36:09 +0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { NavController, Platform } from '@ionic/angular';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  constructor(
+    private platform: Platform,
+    public navCtrl: NavController,
+    private statusBar: Statusbar
+    pritave splashScreen: Splashscreen,
+    private storage: Storage
+  ) {
+    this.initializeApp()
+  }
+
+  async initializeApp() {
+    await this.storage.create();
+
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault()
+      this.splashScreen.hide();
+    })
+
+    this.storage.getItem('isLoggedIn').then((val:any) => {
+      if (!val) {
+        this.navCtrl.navigateRoot('/login')
+      } else {
+        this.navCtrl.navigateRoot('/tabs/tab1')
+      }
+    })
+  }
+}
